8223fab4cea05b132c3239800be7d5af15aeb6b7
Narendra_Sonawane <134570678+Narendra-Sonawane7@users.noreply.github.com>
2026-07-26 00:18:59 +0530

    Improve PDF text extraction and handle empty pages
    Refactor PDF extraction logic to handle empty pages and improve text extraction flow.

--- AI -Research & Knowledge_Assistant/app/services/pdf_service.py
+++ AI -Research & Knowledge_Assistant/app/services/pdf_service.py
@@ -1,34 +1,54 @@
 import fitz  # PyMuPDF
+
 from app.utils.text_utils import chunk_text
-from app.services.ocr_service import (
-    extract_text_from_scanned_pdf
-)
-
-
-
+from app.services.ocr_service import extract_text_from_scanned_pdf
 
 
 def extract_pages_and_chunks(pdf_path: str):
+
     doc = fitz.open(pdf_path)
+
     pages_data = []
     all_chunks = []
 
     for page_num in range(len(doc)):
+
         page = doc[page_num]
-        text = page.get_text()
 
-    if not text.strip():
-        print("Using OCR...")
-        print("=" * 50)
-        print("OCR ACTIVATED")
-        print("=" * 50)
+        text = page.get_text().strip()
 
-        text = extract_text_from_scanned_pdf(pdf_path)
+        # If no text found, use OCR
+        if not text:
 
-        print(text)
+            print("=" * 50)
+            print(f"OCR ACTIVATED FOR PAGE {page_num + 1}")
+            print("=" * 50)
 
-        pages_data.append({"page": page_num + 1, "text": text})
-        for chunk in chunk_text(text):
-            all_chunks.append({"page": page_num + 1, "text": chunk})
+            text = extract_text_from_scanned_pdf(pdf_path).strip()
+
+        # Skip empty pages
+        if not text:
+            continue
+
+        pages_data.append(
+            {
+                "page": page_num + 1,
+                "text": text
+            }
+        )
+
+        chunks = chunk_text(text)
+
+        for chunk in chunks:
+
+            all_chunks.append(
+                {
+                    "page": page_num + 1,
+                    "text": chunk
+                }
+            )
+
+    print(f"Pages Extracted : {len(pages_data)}")
+    print(f"Chunks Created  : {len(all_chunks)}")
 
     return len(doc), pages_data, all_chunks
